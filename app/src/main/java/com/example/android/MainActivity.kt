4c179abd4d38b486e9333c2053515e5be01c0b08
lyma12 <79680072+lyma12@users.noreply.github.com>
2023-10-31 14:15:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.android
+
+import android.os.Bundle
+import android.widget.ListView
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_view)
+
+        var list : ArrayList<Message> = ArrayList<Message>()
+
+        list.add(Message("Nam Dang", "Chai lieu hdh", "10:00 AM"))
+        list.add(Message("Lam Dang", "[JetBrains/compose-multiplatform] Error displaying .xml illustration on Web platform: Uncaught IllegalStateException - Size is unspecified (Issue #3871)", "9:30 AM"))
+        list.add(Message("Pam Dang", "Chai lieu hdh", "9:00 AM"))
+        list.add(Message("Shein", "Chai lieu hdh", "8:40 AM"))
+        list.add(Message("FaceBook", "Chai lieu hdh", "8:30 AM"))
+        list.add(Message("Kam Dang", "KẾT NỐI YÊU THƯƠNG và 7 Nhóm khác là các gợi ý mới dành cho bạn", "8:03 AM"))
+        list.add(Message("Ram Dang", "Dazy x HyunA | Tôi sinh ra đã là người tạo ra xu hướng!", "8:00 AM"))
+        list.add(Message("Pam Dang", "Chai lieu hdh", "9:00 AM"))
+        list.add(Message("Eam Dang", "Chai lieu hdh", "8:40 AM"))
+        list.add(Message("Oleksandr Karpovich", "Chai lieu hdh", "8:30 AM"))
+        list.add(Message("Kam Dang", "Chai lieu hdh", "8:03 AM"))
+        list.add(Message("Ram Dang", "Chai lieu hdh", "8:00 AM"))
+        list.add(Message("Pam Dang", "Chai lieu hdh", "9:00 AM"))
+        list.add(Message("Eam Dang", "Chai lieu hdh", "8:40 AM"))
+        list.add(Message("Iam Dang", "Chai lieu hdh", "8:30 AM"))
+        list.add(Message("Kam Dang", "Chai lieu hdh", "8:03 AM"))
+        list.add(Message("Ram Dang", "Chai lieu hdh", "8:00 AM"))
+
+
+        val listView = findViewById<ListView>(R.id.listView)
+        listView.adapter = Adapter(list)
+    }
+}
+
