f31da735a286f5340b82025dc1c0b1c2b0dee50d
lyma12 <79680072+lyma12@users.noreply.github.com>
2023-11-05 23:08:43 +0700

    update google play store
    05/11/2023

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,38 +1,62 @@
 package com.example.android
 
 import android.os.Bundle
+import android.widget.ListAdapter
 import android.widget.ListView
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.list_view)
+        setContentView(R.layout.main_activity)
+        var list: ArrayList<GroupApp> = ArrayList<GroupApp>()
 
-        var list : ArrayList<Message> = ArrayList<Message>()
+        var listApp: ArrayList<App> = ArrayList<App>()
+        listApp.add(App("Drakrise", R.drawable._55652785_243289621787527_2259314959773497228_n, 4.7))
+        listApp.add(App("Pickle Pete", R.drawable._72615752_192847030479728_144606161263388329_n, 4.6))
+        listApp.add(App("Vampire Survivors", R.drawable._85551498_309214691884071_1295304354531642826_n, 4.0))
+        listApp.add(App("Bad 2 Bad", R.drawable._86442997_742697247688265_3441996262012899685_n, 4.1))
+        listApp.add(App("Chiki's Chase", R.drawable._86442997_742697247688265_3441996262012899685_n, 4.8))
+        list.add(GroupApp("Top-rated games", listApp))
 
-        list.add(Message("Nam Dang", "Chai lieu hdh", "10:00 AM"))
-        list.add(Message("Lam Dang", "[JetBrains/compose-multiplatform] Error displaying .xml illustration on Web platform: Uncaught IllegalStateException - Size is unspecified (Issue #3871)", "9:30 AM"))
-        list.add(Message("Pam Dang", "Chai lieu hdh", "9:00 AM"))
-        list.add(Message("Shein", "Chai lieu hdh", "8:40 AM"))
-        list.add(Message("FaceBook", "Chai lieu hdh", "8:30 AM"))
-        list.add(Message("Kam Dang", "KẾT NỐI YÊU THƯƠNG và 7 Nhóm khác là các gợi ý mới dành cho bạn", "8:03 AM"))
-        list.add(Message("Ram Dang", "Dazy x HyunA | Tôi sinh ra đã là người tạo ra xu hướng!", "8:00 AM"))
-        list.add(Message("Pam Dang", "Chai lieu hdh", "9:00 AM"))
-        list.add(Message("Eam Dang", "Chai lieu hdh", "8:40 AM"))
-        list.add(Message("Oleksandr Karpovich", "Chai lieu hdh", "8:30 AM"))
-        list.add(Message("Kam Dang", "Chai lieu hdh", "8:03 AM"))
-        list.add(Message("Ram Dang", "Chai lieu hdh", "8:00 AM"))
-        list.add(Message("Pam Dang", "Chai lieu hdh", "9:00 AM"))
-        list.add(Message("Eam Dang", "Chai lieu hdh", "8:40 AM"))
-        list.add(Message("Iam Dang", "Chai lieu hdh", "8:30 AM"))
-        list.add(Message("Kam Dang", "Chai lieu hdh", "8:03 AM"))
-        list.add(Message("Ram Dang", "Chai lieu hdh", "8:00 AM"))
+        var listApp1: ArrayList<App> = ArrayList<App>()
+        listApp1.add(App("Shadow Fight 2", R.drawable._76430330_860431435443096_8621011644570302559_n, 4.7))
+        listApp1.add(App("Chess", R.drawable._85532808_712231947075953_6114396240778827804_n, 4.6))
+        listApp1.add(App("Mindustry", R.drawable._85532808_712231947075953_6114396240778827804_n, 4.0))
+        listApp1.add(App("Stick War", R.drawable._85551498_309214691884071_1295304354531642826_n, 4.1))
+        listApp1.add(App("Chase", R.drawable._85532808_712231947075953_6114396240778827804_n, 4.8))
+        list.add(GroupApp("Offline games", listApp1))
 
+        var listApp2: ArrayList<App> = ArrayList<App>()
+        listApp2.add(App("Rise", R.drawable._72615752_192847030479728_144606161263388329_n, 4.7))
+        listApp2.add(App("Pete", R.drawable._76430330_860431435443096_8621011644570302559_n, 4.6))
+        listApp2.add(App("Survivors", R.drawable._55652785_243289621787527_2259314959773497228_n, 4.0))
+        listApp2.add(App("Bad 2 Bad", R.drawable._85532808_712231947075953_6114396240778827804_n, 4.1))
+        listApp2.add(App("Chiki's", R.drawable._85551498_309214691884071_1295304354531642826_n, 4.8))
+        list.add(GroupApp("Stylized games", listApp2))
 
-        val listView = findViewById<ListView>(R.id.listView)
-        listView.adapter = Adapter(list)
+        var listApp3: ArrayList<App> = ArrayList<App>()
+        listApp3.add(App("Drakrise", R.drawable._86442997_742697247688265_3441996262012899685_n, 4.7))
+        listApp3.add(App("Pickle Pete", R.drawable._55652785_243289621787527_2259314959773497228_n, 4.6))
+        listApp3.add(App("Vampire Survivors", R.drawable._86442997_742697247688265_3441996262012899685_n, 4.0))
+        listApp3.add(App("Bad 2 Bad", R.drawable._76430330_860431435443096_8621011644570302559_n, 4.1))
+        listApp3.add(App("Chiki's Chase", R.drawable._55652785_243289621787527_2259314959773497228_n, 4.8))
+        list.add(GroupApp("Roguelike games", listApp3 ))
+
+        val customAdapter: GroupAdapater = GroupAdapater(list, this)
+
+        val listView = findViewById<RecyclerView>(R.id.recyclerViewGroup)
+        listView.adapter = customAdapter
+        listView.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
     }
+
 }
 
